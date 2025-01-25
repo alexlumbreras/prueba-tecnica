bfed7c863590d21a1442bb4d08b99b2821044bf2
Alejandro Lumbreras López <alexlumbreras@MacBook-Pro-de-Alejandro.local>
2025-01-25 19:17:47 +0100

    refactor: remove unnecessary files and clean up project structure

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './style.css';
 import { App } from './App/App';
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
