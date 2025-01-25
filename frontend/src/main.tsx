e4087168f3074b465b9e5e998ffec81dd60338a3
Alejandro Lumbreras López <alexlumbreras@MacBook-Pro-de-Alejandro.local>
2025-01-25 13:34:11 +0100

    chore: integrate React and TypeScript into Vite vanilla project

---
+++ frontend/src/main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './style.css';
+
+const App = () => {
+  return (
+      <h1>Hello, React with Vite and TypeScript!</h1>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
