ad276dbb5f8f74a5b3f324201b9ed25443319464
Alejandro Lumbreras López <alexlumbreras@MacBook-Pro-de-Alejandro.local>
2025-01-27 21:24:48 +0100

    test: add tests for all guard clauses in USCovidDashboard view

--- frontend/src/components/USCovidDashboard/__tests__/USCovidDashboard.test.tsx
+++ frontend/src/components/USCovidDashboard/__tests__/USCovidDashboard.test.tsx
@@ -1,16 +1,45 @@
-import { describe, expect, it } from 'vitest';
+import { beforeEach, describe, expect, it, vi } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import { USCovidDashboard } from '../USCovidDashboard';
+import * as covidService from '../../../application/services/covidService';
 
 describe('USCovidDashboard Component', () => {
-  it('should render title', () => {
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+
+  it('should render title', async () => {
     render(<USCovidDashboard />);
 
     expect(
-      screen.getByRole('heading', {
+      await screen.findByRole('heading', {
         level: 1,
         name: 'COVID Data in the US 🦠',
       })
     ).toBeVisible();
+  });
+
+  it('should display no data message when data is empty', async () => {
+    vi.spyOn(covidService, 'getCovidStats').mockResolvedValueOnce([]);
+
+    render(<USCovidDashboard />);
+
+    expect(await screen.findByText('No data')).toBeVisible();
+  });
+
+  it('should display loading message when data is loading', () => {
+    vi.spyOn(covidService, 'getCovidStats').mockResolvedValueOnce([]);
+
+    render(<USCovidDashboard />);
+
+    expect(screen.getByText('Loading data...')).toBeVisible();
+  });
+
+  it('should display error message when data fetch fails', async () => {
+    vi.spyOn(covidService, 'getCovidStats').mockRejectedValueOnce(new Error());
+
+    render(<USCovidDashboard />);
+
+    expect(await screen.findByText('An error occurred!')).toBeVisible();
   });
 });
