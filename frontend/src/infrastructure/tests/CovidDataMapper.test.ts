fb36ffd7575446ab5ab00ec4d64461c01d2ebe16
Alejandro Lumbreras López <alexlumbreras@MacBook-Pro-de-Alejandro.local>
2025-01-27 19:12:14 +0100

    test: create builder to test mapper and ensure correct mapping in infrastructure layer

---
+++ frontend/src/infrastructure/tests/CovidDataMapper.test.ts
@@ -0,0 +1,46 @@
+import { describe, expect, it } from 'vitest';
+import { CovidStatsDTOBuilder } from '../api/covid/builders/CovidStatsDTOBuilder';
+import { CovidStats } from '../../domain/models/CovidStats';
+import { mapCovidData } from '../api/covid/mappers/CovidDataMapper';
+
+describe('mapCovidData', () => {
+  it('should correctly map Stats to CovidStats', () => {
+    const inputData = new CovidStatsDTOBuilder()
+      .setDate('2025-01-01')
+      .setCases(100000)
+      .setTesting(200000)
+      .setDeaths(1500)
+      .build();
+
+    const expectedResult: CovidStats = {
+      date: '2025-01-01',
+      cases: 100000,
+      deaths: 1500,
+      tests: 200000,
+    };
+
+    const result = mapCovidData(inputData);
+
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('should return default values when values are undefined', () => {
+    const inputData = new CovidStatsDTOBuilder()
+      .setDate('2025-01-01')
+      .setCases(0)
+      .setTesting(0)
+      .setDeaths(0)
+      .build();
+
+    const expectedResult: CovidStats = {
+      date: '2025-01-01',
+      cases: 0,
+      deaths: 0,
+      tests: 0,
+    };
+
+    const result = mapCovidData(inputData);
+
+    expect(result).toEqual(expectedResult);
+  });
+});
