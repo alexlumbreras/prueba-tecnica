e1d2db9bd8ca2369b16ee13742b810875c2b2aad
Alejandro Lumbreras López <alexlumbreras@MacBook-Pro-de-Alejandro.local>
2025-01-27 21:09:59 +0100

    feat: use Covid stats service in the view and render content

--- frontend/src/components/USCovidDashboard/USCovidDashboard.tsx
+++ frontend/src/components/USCovidDashboard/USCovidDashboard.tsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { getCovidStats } from '../../application/services/covidService';
+import { CovidStats } from '../../domain/models/CovidStats';
 
 export const USCovidDashboard: React.FC = () => {
-  return <h1>COVID Data in the US 🦠</h1>;
+  const [covidData, setCovidData] = useState<CovidStats[] | undefined>(
+    undefined
+  );
+  const [hasError, setHasError] = useState<boolean>(false);
+
+  useEffect(() => {
+    const onLoad = async () => {
+      try {
+        setCovidData(await getCovidStats());
+      } catch (error) {
+        setHasError(true);
+      }
+    };
+
+    onLoad();
+  }, []);
+
+  if (hasError) {
+    return <div>An error occurred!</div>;
+  }
+
+  if (covidData === undefined) {
+    return <div>Loading data...</div>;
+  }
+
+  return (
+    <div>
+      <h1>COVID Data in the US 🦠</h1>
+      {covidData.map((data) => (
+        <div key={data.date}>
+          <p>Date: {data.date}</p>
+          <p>Cases: {data.cases}</p>
+          <p>Deaths: {data.deaths}</p>
+          <p>Tests: {data.tests}</p>
+        </div>
+      ))}
+    </div>
+  );
 };
