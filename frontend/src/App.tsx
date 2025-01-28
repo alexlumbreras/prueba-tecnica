2ebd28bd9589477917d91f8490b37f2be50f105c
Alejandro Lumbreras López <alexlumbreras@MacBook-Pro-de-Alejandro.local>
2025-01-28 21:50:31 +0100

    feat: abstract NameForm into a component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { HomePage } from './views/HomePage/HomePage';
-import { USCovidDashboard } from './components/USCovidDashboard/USCovidDashboard';
+import { USCovidDashboard } from './views/USCovidDashboard/USCovidDashboard';
+import { NameForm } from './views/NameForm/NameForm';
 
 export const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/exercise1" element={<>Exercise 1</>} />
+        <Route path="/exercise1" element={<NameForm />} />
         <Route path="/exercise2" element={<USCovidDashboard />} />
       </Routes>
     </Router>
---
+++ frontend/src/views/NameForm/NameForm.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const NameForm: React.FC = () => {
+  return <>Exercise 1</>;
+};
